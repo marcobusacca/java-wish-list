3f739829389b8ffaf09467a047a74a280747d97c
marcobusacca <marcobusacca01@gmail.com>
2023-11-17 12:30:59 +0100

    Esercizio Completato

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,9 +1,43 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Scanner in = new Scanner(System.in);
+		
+		List<String> listaRegali = new ArrayList<>();
+		
+		while (true) {
+			
+			System.out.print("Inserisci il nome del regalo: ");
+			String nomeRegalo = in.nextLine();
+			
+			listaRegali.add(nomeRegalo);
+			
+			System.out.println("\nLunghezza della lista: " + listaRegali.size() + "\n");
+			
+			System.out.print("Vuoi inserire un altro regalo? (si | no): ");
+			String input = in.nextLine().toLowerCase();
+			
+			if (input.equals("no")) {
+				break;
+			}
+		}
+		
+		Collections.sort(listaRegali);
+		
+		System.out.println("\n" + listaRegali);
+		
+		for (String regalo : listaRegali) {
+			System.out.println(regalo);
+		}
+		
+		in.close();
 	}
 }
